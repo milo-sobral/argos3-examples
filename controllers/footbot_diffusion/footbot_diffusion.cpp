70f58289748535beb4ff76ab10ef57b94a546711
Milo <milosobral@gmail.com>
2021-01-20 18:18:13 +0100

    Updated strategy

--- controllers/footbot_diffusion/footbot_diffusion.cpp
+++ controllers/footbot_diffusion/footbot_diffusion.cpp
@@ -5,7 +5,9 @@
 /* 2D vector definition */
 #include <argos3/core/utility/math/vector2.h>
 
-#define STRATEGY false
+#define STRATEGY true
+#define MAX_VEL 40.0f
+#define MIN_VEL 2.0f
 
 /****************************************/
 /****************************************/
@@ -14,7 +16,7 @@ CFootBotDiffusion::CFootBotDiffusion() :
    m_pcWheels(NULL),
    m_pcProximity(NULL),
    m_cAlpha(10.0f),
-   m_fDelta(0.5f),
+   m_fDelta(0.01f),
    m_fWheelVelocity(2.5f),
    m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
                            ToRadians(m_cAlpha)) {}
@@ -94,7 +96,52 @@ void CFootBotDiffusion::ControlStep() {
         }
      }
    } else {
-     // Insert en straetgy
+     /* Get readings from proximity sensor */
+     const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+     /* Sum them together */
+     CVector2 cAccumulator;
+     for(size_t i = 0; i < tProxReads.size(); ++i) {
+        cAccumulator += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+     }
+     cAccumulator /= tProxReads.size();
+     /* If the angle of the vector is small enough and the closest obstacle
+      * is far enough, continue going straight, otherwise curve a little
+      */
+     CRadians cAngle = cAccumulator.Angle();
+     if(m_cGoStraightAngleRange.WithinMinBoundIncludedMaxBoundIncluded(cAngle) &&
+        cAccumulator.Length() < m_fDelta ) {
+
+        // This condition is added to avoid the algorithm not detecting obstacles when the robot is heading stratight onto them.
+        if (cAccumulator.GetX() != 0.0f) {
+          m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity * 0.9f);
+        } else {
+          /* Go straight */
+          // If we do not detect obstacles, we can accelerate up to a maximum velocity.
+          m_fWheelVelocity = 1.5f * m_fWheelVelocity;
+          m_fWheelVelocity = (m_fWheelVelocity > MAX_VEL) ? MAX_VEL : m_fWheelVelocity;
+          m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
+        }
+
+     }
+     else {
+        // If we detect an obstacle, we slow down up to a minimum Velocity.
+        m_fWheelVelocity = 0.95f * m_fWheelVelocity;
+        m_fWheelVelocity = (m_fWheelVelocity < MIN_VEL) ? MIN_VEL : m_fWheelVelocity;
+        /* Turn, depending on the sign of the angle */
+
+        // If the obstacles are mostly behind us, we don't need to turn as much
+        if (abs(cAngle.GetValue()) > 2.0f) {
+          m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity * 0.8f);
+        }
+        // Else, turn around to get "as far as possible" from the obstcale.
+        // The damping factor of 0.8 is to avoid completely stopping to turn around. 
+        else if(cAngle.GetValue() > 0.0f) {
+           m_pcWheels->SetLinearVelocity(m_fWheelVelocity, -m_fWheelVelocity * 0.8f);
+        }
+        else {
+           m_pcWheels->SetLinearVelocity(-m_fWheelVelocity * 0.8f, m_fWheelVelocity);
+        }
+     }
    }
 
 }
